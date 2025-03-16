48b3dc7ab00ad5919d7653a7ef7de7e9f2ae9792
ShubhamMayane <shubhammayane@gmail.com>
2025-03-16 12:02:28 +0530

    Common Component Generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServicesComponent } from './services/services.component';
+
+
+const routes: Routes = [
+{path:"home",component:HomeComponent},
+{path:"aboutus",component:AboutusComponent},
+{path:"services",component:ServicesComponent},
+{path:"",redirectTo:"/home",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './commonComponents/navbar/navbar.component';
+import { FooterComponent } from './commonComponents/footer/footer.component';
+import { HomeModule } from './home/home.module';
+import { AboutusModule } from './aboutus/aboutus.module';
+import { ServicesModule } from './services/services.module';
+import { BannerComponent } from './commonComponents/banner/banner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    AboutusModule,
+    ServicesModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
